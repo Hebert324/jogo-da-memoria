e81d8a96ac244ab56576c9d23a02a9140cd6a59a
Hebert324 <hebertrocha76@gmail.com>
2021-05-27 09:47:38 -0300

    javacript

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const FRONT = "card_font"
+const BACK = "card_back"
+
+let apps = [
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery',
+    'mongo',
+    'node',
+    'react'
+]
